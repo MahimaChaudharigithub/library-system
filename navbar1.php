d8f7d17e6495510f76ccc129984a72baa3bca336
Mahimagitup <98876333+Mahimagitup@users.noreply.github.com>
2023-06-23 17:36:02 +0530

    navbar1.php

---
+++ navbar1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap-4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <span class="navbar-brand mb-0 h1">Library</span>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a href="index.php" class="nav-link">
+                    User Login
+                </a>
+
+            </li>
+            <li class="nav-item active">
+                <a href="adminlogin.php" class="nav-link">
+                   Admin Login 
+                </a>
+                
+            </li>
+            <li class="nav-item active">
+                <a href="admin register.php" class="nav-link">
+                    Admin Register
+                </a>
+                
+            </li>
+        </ul>
+    </div>
+    </nav>
+    
+</body>
+</html>
