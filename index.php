3bd98133ee4e4e08431b23cd7b924afeea174fc9
Mahimagitup <98876333+Mahimagitup@users.noreply.github.com>
2023-06-23 17:24:00 +0530

    index.php

--- index.php
+++ index.php
@@ -1,31 +1,52 @@
 <?php
-include 'header.php';
+include 'navbar1.php';
 include 'connect.php';?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<link style="stylesheet" href="style1.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="bootstrap-4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    
+</head>
 <style>
-
-</style></head>
-<body>
-
-
-<form id="login" method="post" action="login.php">
-
-<center><h2 style="color:blue">user login</h2></center>
-<label>Roll NO </label><br>
-<input type="text" class="form-control" name="rollno"  placeholder="Roll no" required><br>
-</div><br>
-<label>Password </label><br>
-<input type="password" class="form-control" name="pass" max=6    placeholder="password" required>
-
-</div>
- <button type="submit" class="form-control"   name="login" value ="login">login</button>
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+      background-image: url("image/library.jpg");
 
 
-</form>
+  }
+</style>
+<body >
+ <section>
+    <div class="container">
+        <div class="row">
+            <div class="col 12 col-sm-8 col-md-6 m-auto ">
+                <div class="card" style="margin-top:50px;">
+                    <div class="card-title offset-sm-5">
+                        <h3>Login</h3>
+                    </div>
 
-</div>
+                    <div class="card-body">
+                        <form action="login.php" method="post" id="login">
+
+                            <input type="text" name="rollno" id="" class="form-control my-3 py-2" placeholder="Roll no" required>
+                            <input type="text" name="pass" id="" class="form-control my-3 py-2" placeholder="Password" required>
+                            <div class="text-center mt-3">
+                                <button class="btn btn-primary">Login</button>
+                            </div>
+                            <p class=" mt-2" style="margin-left: 100px;">Alrady have an account?<a href="register.php" style="margin-left: 10px;" >
+
+                            Sign Up</a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </body>
 </html>
